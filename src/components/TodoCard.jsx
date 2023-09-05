d2656bd44a8b7f87f0297f2fd7439068d5539374
on3ss <on3ss@DESKTOP-UM81EHJ>
2023-09-05 17:03:11 +0530

    aligned checkbox

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -17,7 +17,7 @@ export default function TodoCard({ todo }) {
                 <h4 className="text-xl text-gray-800 line-clamp-2">{todo.title}</h4>
                 <p className="mt-2 text-gray-600 line-clamp-2">{todo.description}</p>
 
-                <div className="flex justify-between">
+                <div className="flex items-center justify-between">
                     <div className="flex justify-start gap-2 my-2">
                         {
                             todo.categories.map((category, key) => {
@@ -27,9 +27,8 @@ export default function TodoCard({ todo }) {
                             })
                         }
                     </div>
-
                     <div>
-                        <input type="checkbox" className="w-4 h-4 text-blue-600 bg-transparent border-2 border-gray-300 rounded focus:ring-blue-500 focus:ring-2" checked={isDone} onChange={toggleIsDone} />
+                        <input type="checkbox" className="w-4 h-4 text-blue-400 bg-transparent border-2 border-gray-300 rounded focus:ring-blue-300 focus:ring-2" checked={isDone} onChange={toggleIsDone} />
                         <label className="ml-1 text-sm">Done</label>
                     </div>
                 </div>
