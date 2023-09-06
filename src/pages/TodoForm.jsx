e6708c91611c759d298a8f8200d84e1326877624
on3ss <on3ss@DESKTOP-UM81EHJ>
2023-09-06 12:19:28 +0530

    added reducers and used in form

--- src/pages/TodoForm.jsx
+++ src/pages/TodoForm.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
-import categories from "../mock/todoCategory";
+import defaultCategories from "../mock/todoCategory";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../store/slice/todoSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function TodoForm() {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     // TODO: Validate and submit using Form Hooks package
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
+    const [categories, setCategories] = useState([])
+
+    const handleSelectChange = (e) => {
+        const updatedOptions = [...e.target.options].filter(option => option.selected).map(x => x.value)
+        setCategories(updatedOptions)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addTodo({ title, description, categories }))
+        return navigate('/')
+    }
+
     return (
-        <div className="mt-4">
+        <form className="mt-4" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
                 <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" value={title} onChange={(e) => setTitle(e.target.value)} required />
@@ -18,10 +38,10 @@ export default function TodoForm() {
 
             <div className="mt-4">
                 <label htmlFor="categories" className="block mb-2 text-sm font-medium text-gray-900">Select an option</label>
-                <select multiple id="categories" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <select multiple id="categories" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={categories} onChange={handleSelectChange}>
                     <option disabled>Choose categories</option>
                     {
-                        categories.map((category, key) => {
+                        defaultCategories.map((category, key) => {
                             return (
                                 <option key={key} value={key}>{category.label}</option>
                             )
@@ -33,6 +53,6 @@ export default function TodoForm() {
             <div className="mt-4">
                 <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Save</button>
             </div>
-        </div>
+        </form>
     )
 }
---
+++ src/store/slice/todoSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState: {
+        todos: []
+    },
+    reducers: {
+        addTodo: {
+            reducer: (state, action) => {
+                state.todos.push(action.payload)
+            },
+            prepare: (todo) => {
+                const id = nanoid()
+                return {
+                    payload: {
+                        id,
+                        ...todo
+                    }
+                }
+            }
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((item) => item.id !== action.payload.id)
+        },
+        toggleDone: (state, action) => {
+            state.todos = state.todos.map((item) => {
+                if (item.id === action.payload.id) {
+                    item.isDone = !item.isDone
+                }
+            })
+        }
+    }
+})
+
+export const { addTodo, removeTodo, toggleDone } = todoSlice.actions
+
+export default todoSlice.reducer
