b070a54c9edc3bafd30a47df5bc4927a4e6aa9b7
on3ss <on3ss@DESKTOP-UM81EHJ>
2023-09-11 11:50:35 +0530

    added some responsive behaviour

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -29,19 +29,19 @@ export default function TodoList() {
     }, [filter, hideDone, todos])
 
     return (
-        <>
+        <div className="md:flex md:justify-start">
             <nav className="py-4 overflow-x-auto">
-                <ul className="flex justify-start">
-                    <li>
-                        <button className={filter == VIEW_ALL_FILTER_VALUE ? "flex gap-1 mx-2 px-4 py-2 align-middle bg-gray-200 rounded" : "flex gap-1 px-4 py-2 align-middle rounded"} onClick={() => setFilter(VIEW_ALL_FILTER_VALUE)}>
+                <ul className="flex justify-start md:flex-col">
+                    <li className="mx-2">
+                        <button className={filter == VIEW_ALL_FILTER_VALUE ? "flex gap-1 px-4 py-2 align-middle bg-gray-200 rounded md:w-full" : "flex gap-1 px-4 py-2 align-middle rounded md:w-full"} onClick={() => setFilter(VIEW_ALL_FILTER_VALUE)}>
                             <span>All</span>
                         </button>
                     </li>
                     {
                         categories.map((category, index) => {
                             return (
-                                <li key={index}>
-                                    <button className={filter == index ? "flex gap-1 px-4 py-2 align-middle bg-gray-200 rounded" : "flex gap-1 px-4 py-2 align-middle rounded"} onClick={() => setFilter(index)}>
+                                <li key={index} className="mx-2">
+                                    <button className={filter == index ? "flex gap-1 px-4 py-2 align-middle bg-gray-200 rounded md:w-full" : "flex gap-1 px-4 py-2 align-middle rounded md:w-full"} onClick={() => setFilter(index)}>
                                         <div className={"w-6 h-6 rounded-full " + category.color}></div>
                                         <span>{category.label}</span>
                                     </button>
@@ -52,27 +52,29 @@ export default function TodoList() {
                 </ul>
             </nav>
 
-            <div className="px-4 my-2">
-                <div>
-                    <input type="checkbox" className="w-4 h-4 text-blue-400 bg-transparent border-2 border-gray-300 rounded focus:ring-blue-300 focus:ring-2" checked={hideDone} onChange={() => setHideDone(!hideDone)} />
-                    <label className="ml-1 text-sm">Hide Done</label>
+            <div className="md:flex-1">
+                <div className="px-4 my-2">
+                    <div>
+                        <input type="checkbox" className="w-4 h-4 text-blue-400 bg-transparent border-2 border-gray-300 rounded focus:ring-blue-300 focus:ring-2" checked={hideDone} onChange={() => setHideDone(!hideDone)} />
+                        <label className="ml-1 text-sm">Hide Done</label>
+                    </div>
+                </div>
+
+                <div className="px-4 mt-4">
+                    <ul>
+                        {
+                            filteredTodos && filteredTodos.length > 0
+                                ? filteredTodos.map((todo, key) => {
+                                    return (<TodoCard key={todo.id} todo={todo} />)
+                                }) : (
+                                    <div className="flex justify-center">
+                                        <h5>No todos yet!</h5>
+                                    </div>
+                                )
+                        }
+                    </ul>
                 </div>
             </div>
-
-            <div className="px-4 mt-4">
-                <ul>
-                    {
-                        filteredTodos && filteredTodos.length > 0
-                            ? filteredTodos.map((todo, key) => {
-                                return (<TodoCard key={todo.id} todo={todo} />)
-                            }) : (
-                                <div className="flex justify-center">
-                                    <h5>No todos yet!</h5>
-                                </div>
-                            )
-                    }
-                </ul>
-            </div>
-        </>
+        </div>
     )
 }
